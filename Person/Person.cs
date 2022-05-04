9c001a26b98d5b87b0bebdf601221b76b57e585d
Ярослав Проценко <yaroslav.protsenko@ukma.edu.ua>
2022-05-04 13:22:24 +0200

    Практична 3

--- Person/Person.cs
+++ Person/Person.cs
@@ -10,6 +10,12 @@ namespace Person
     {
         public Person(string name, string surname, string email, DateTime dateBirth)
         {
+            //Викликаємо перевірку для дати
+            CheckFutureAndPastDate(dateBirth);
+
+            //Викликаємо перевірку для email
+            CheckEmail(email);
+
             Name = name;
             Surname = surname;
             Email = email;
@@ -18,13 +24,15 @@ namespace Person
 
         public Person(string name, string surname, string email)
         {
-            Name=name;
-            Surname=surname;
-            Email=email;
+            CheckEmail(email);
+            Name = name;
+            Surname = surname;
+            Email = email;
         }
 
         public Person(string name, string surname, DateTime dateBirth)
         {
+            CheckFutureAndPastDate(dateBirth);
             Name = name;
             Surname = surname;
             DateBirth = dateBirth;
@@ -34,7 +42,7 @@ namespace Person
         {
             get
             {
-                if((DateTime.Now.Year - DateBirth.Year) >= 18)
+                if ((DateTime.Now.Year - DateBirth.Year) >= 18)
                 {
                     return true;
                 }
@@ -42,76 +50,38 @@ namespace Person
             }
         }
 
-        public string SunSign 
+        public string SunSign
         {
             get
             {
                 switch (DateBirth.Month)
                 {
                     case 1:
-                        {
-                            return "Goat";
-                            break;
-                        }
+                        return "Goat";
                     case 2:
-                        {
-                            return "Water Bearer";
-                            break;
-                        }
+                        return "Water Bearer";
                     case 3:
-                        {
-                            return "Fish";
-                            break;
-                        }
+                        return "Fish";
                     case 4:
-                        {
-                            return "Ram";
-                            break;
-                        }
+                        return "Ram";
                     case 5:
-                        {
-                            return "Bull";
-                            break;
-                        }
+                        return "Bull";
                     case 6:
-                        {
-                            return "Twins";
-                            break;
-                        }
+                        return "Twins";
                     case 7:
-                        {
-                            return "Crab";
-                            break;
-                        }
+                        return "Crab";
                     case 8:
-                        {
-                            return "Lion";
-                            break;
-                        }
+                        return "Lion";
                     case 9:
-                        {
-                            return "Virgin";
-                            break;
-                        }
+                        return "Virgin";
                     case 10:
-                        {
-                            return "Balance";
-                            break;
-                        }
+                        return "Balance";
                     case 11:
-                        {
-                            return "Scorpion";
-                            break;
-                        }
+                        return "Scorpion";
                     case 12:
-                        {
-                            return "Archer";
-                            break;
-                        }
+                        return "Archer";
                     default:
-                        {
-                            return "None";
-                        }
+                        return "None";
                 }
             }
         }
@@ -121,45 +91,26 @@ namespace Person
             get
             {
                 if (DateBirth.Year % 10 == 1)
-                {
                     return "Tiger";
-                }
                 if (DateBirth.Year % 10 == 2)
-                {
                     return "Rabbit";
-                }
                 if (DateBirth.Year % 10 == 3)
-                {
                     return "Dragon";
-                }
                 if (DateBirth.Year % 10 == 4)
-                {
                     return "Snake";
-                }
-                if (DateBirth.Year % 10 == 5    )
-                {
+                if (DateBirth.Year % 10 == 5)
                     return "Horse";
-                }
                 if (DateBirth.Year % 10 == 6)
-                {
                     return "Goat";
-                }
                 if (DateBirth.Year % 10 == 7)
-                {
                     return "Monkey";
-                }
                 if (DateBirth.Year % 10 == 8)
-                {
                     return "Rooster";
-                }
                 if (DateBirth.Year % 10 == 9)
-                {
                     return "Dog";
-                }
                 if (DateBirth.Year % 10 == 0)
-                {
                     return "Pig";
-                }
+
                 return "None";
             }
         }
@@ -174,7 +125,24 @@ namespace Person
 
         public override string ToString()
         {
-            return  "Name: " + Name + "\nSurname: " + Surname + "\nEmail: " + Email + "\nDateBirth" + DateBirth + "\nIsAdult: " + IsAdult + "\nSunSign: " + SunSign + "\nChinese Sign: " + ChineseSign + "\nIsBirtday: " + IsBirthday + "\n"; 
+            return "Name: " + Name + "\nSurname: " + Surname + "\nEmail: " + Email + "\nDateBirth" + DateBirth + "\nIsAdult: " + IsAdult + "\nSunSign: " + SunSign + "\nChinese Sign: " + ChineseSign + "\nIsBirtday: " + IsBirthday + "\n";
+        }
+
+        private void CheckFutureAndPastDate(DateTime dateTime)
+        {
+            //Якщо дата більша за сьогоднішню, то це помилка
+            if (dateTime.Date > DateTime.Now)
+                throw new FutureDate();
+
+            //Якщо дата менша за сьогодняшну на 130 років - це помилка
+            if (dateTime.Year < DateTime.Now.Year - 130)
+                throw new PastDate();
+        }
+
+        private void CheckEmail(string email)
+        {
+            if (email != "joeschmoe@mydomain.com")
+                throw new EmailExeption();
         }
 
         public string Name { get; set; }
@@ -184,5 +152,56 @@ namespace Person
         public string Email { get; set; }
 
         public DateTime DateBirth { get; set; }
+    }
+
+    class FutureDate : Exception
+    {
+        public DateTime ErrorCode { get { return errorCode; } }
+
+        private DateTime errorCode;
+
+        public FutureDate(DateTime birthYear, string message = "Дата народження у майбутньому") : base(message)
+        {
+            //Зберігаємо код помилки(неправильну дату)
+            errorCode = birthYear;
+        }
+
+        public FutureDate(string message = "Дата народження у майбутньому") : base(message)
+        {
+        }
+    }
+
+    class PastDate : Exception
+    {
+        public DateTime ErrorCode { get { return errorCode; } }
+
+        private DateTime errorCode;
+
+        public PastDate(DateTime birthYear, string message = "Дата народження дуже давня") : base(message)
+        {
+            //Зберігаємо код помилки(неправильну дату)
+            errorCode = birthYear;
+        }
+
+        public PastDate(string message = "Дата народження дуже давня") : base(message)
+        {
+        }
+    }
+
+    class EmailExeption : Exception
+    {
+        public string ErrorCode { get { return errorCode; } }
+
+        private string errorCode;
+
+        public EmailExeption(string email, string message = "Невірна пошта") : base(message)
+        {
+            //Зберігаємо код помилки(неправильний email)
+            errorCode = email;
+        }
+
+        public EmailExeption(string message = "Невірна пошта") : base(message)
+        {
+        }
     }
 }
